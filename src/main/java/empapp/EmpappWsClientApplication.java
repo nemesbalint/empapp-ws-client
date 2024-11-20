1e23578dbd852f519f84f096f64ba1fa82923f3b
Alerant_NemesB <balint.nemes@erstebank.hu>
2024-11-20 12:01:50 +0100

    Commandline ws hívás

--- src/main/java/empapp/EmpappWsClientApplication.java
+++ src/main/java/empapp/EmpappWsClientApplication.java
@@ -1,13 +1,22 @@
 package empapp;
 
+import com.training360.services.empapp.EmployeeWebServiceService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmpappWsClientApplication {
+public class EmpappWsClientApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EmpappWsClientApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EmpappWsClientApplication.class, args);
+    }
 
+    @Override
+    public void run(String... args) throws Exception {
+        var service = new EmployeeWebServiceService();
+        var port = service.getEmployeeWebServicePort();
+        var response = port.findAllEmployees();
+        response.forEach(e -> System.out.println(e.getName()));
+    }
 }
--- src/test/java/empapp/EmpappWsClientApplicationTests.java
+++ src/test/java/empapp/EmpappWsClientApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EmpappWsClientApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
